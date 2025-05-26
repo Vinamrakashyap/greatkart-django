bbcdc4d0cdb0d2cf5b503e4190e804dc225fe1ed
Vinamra <kumarvinamra61@gmail.com>
2025-05-26 13:43:02 +0530

    Initial commit for 4th Django project: greatkart

---
+++ store/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render , get_object_or_404
+from . import views
+from .models import product
+from category.models import category
+# Create your views here.
+
+def store(request , category_slug = None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(category , slug = category_slug)
+        products = product.objects.filter(category = categories , is_available = True)
+        product_count = products.count()
+
+    else:
+        products = product.objects.all().filter(is_available = True)
+        product_count = products.count()
+    
+
+    context = {
+        'products' : products ,
+        'product_count' : product_count ,
+    }
+    return render(request , 'store/store.html',context)
+
+def product_detail(request , category_slug , product_slug):
+    single_product = get_object_or_404(product, category__slug=category_slug, slug=product_slug)
+    context = {
+        'product': single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
